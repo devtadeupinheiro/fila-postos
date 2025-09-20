d4c5e19e424ccda9f8ce0d567047f1dd554c3f49
Tadeu Pinheiro <devtadeupinheiro@gmail.com>
2025-09-20 11:11:32 -0300

    Project created

---
+++ src/test/java/dev/tadeupinheiro/filapostos/FilaPostosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tadeupinheiro.filapostos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilaPostosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
